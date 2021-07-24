c660528a77c16ec83b44cedd64625cabcce931a2
Connor <cmstroh99@gmail.com>
2021-07-24 16:13:49 -0400

    added lots
    added lots

---
+++ src/env.d.ts
@@ -0,0 +1,6 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    DB_USER: string;
+    DB_PASSWORD: string;
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
-console.log("hello world")
+import "reflect-metadata";
+import "dotenv-safe/config";
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import mikroConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { UserResolver } from "./resolvers/user";
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroConfig);
+  await orm.getMigrator().up();
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, UserResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("Server started on localhost:4000");
+  });
+};
+
+main().catch((err) => {
+  console.log(err);
+});
