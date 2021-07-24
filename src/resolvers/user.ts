3e498d363f0c15032e81f254ab298147579a2cb4
Connor <cmstroh99@gmail.com>
2021-07-24 16:16:17 -0400

    Update user.ts

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,7 +1,7 @@
-import { User } from "src/entities/User";
-import { MyContext } from "src/types";
+import { MyContext } from "../types";
 import { Arg, Ctx, Field, InputType, Mutation, Resolver } from "type-graphql";
 import argon2 from "argon2";
+import { User } from "../entities/User";
 
 @InputType()
 class UsernamePasswordInput {
